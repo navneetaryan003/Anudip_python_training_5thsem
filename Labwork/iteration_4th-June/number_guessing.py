cf5b1bec640cc24f53fbe6e8af3f8a9c310486ac
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 16:25:26 +0530

    number guessing problem

--- Labwork/iteration_4th-June/number_guessing.py
+++ Labwork/iteration_4th-June/number_guessing.py
@@ -1,11 +1,25 @@
+#taking input of secret number
 secret_number=int(input("enter the secret number"))
+
 attempt=0
+
 while (True):
+
+    #taking guess number from the user 
+
     guess_number=int(input("enter the number you guess"))
+
+    #increment attempt whenever new guess number entered
+
     attempt+=1
+
+    #checking if the number is same or not 
     if(secret_number==guess_number):
         print("correct guess")
         break
+
+    #giving clue to the user whether it is high or low 
+    
     if(guess_number<secret_number):
         print("too low")
     else:
