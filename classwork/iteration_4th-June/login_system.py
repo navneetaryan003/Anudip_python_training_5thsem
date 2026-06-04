127e2390771acda85a6e20f6f910f6c255783647
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 21:06:41 +0530

    login system

--- classwork/iteration_4th-June/login_system.py
+++ classwork/iteration_4th-June/login_system.py
@@ -1,7 +1,13 @@
-correct_password="admin123"
+#validation of password 
+
+correct_password="admin123"    #initialisation of correct password 
+
 while(True):
-    password=input("enter password")
-    if(password==correct_password):
+
+    password=input("enter password")  #password enter by the user for validation
+
+    if(password==correct_password):   #checking whether the password is matched or not 
+        
         print("login successful")
         break
     else:
