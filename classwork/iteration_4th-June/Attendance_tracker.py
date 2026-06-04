70cb37ed70c73294e1b89d0edaac738c57229358
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 21:01:31 +0530

    attendance tracker

--- classwork/iteration_4th-June/Attendance_tracker.py
+++ classwork/iteration_4th-June/Attendance_tracker.py
@@ -1,8 +1,9 @@
 #write a program to count the no of student present or absent
 
-total_students=30
-attendance_count=1
-present_count=0
+
+total_students=30      #initialisation of number of student
+attendance_count=1     #attendance counter
+present_count=0        #present student record counter
 
 while(attendance_count<=30):
     
@@ -13,13 +14,14 @@ while(attendance_count<=30):
 
     #count the present student
 
-    if(student_present.lower()=="present"):
+    if(student_present.lower()=="present"):  #lower function is to convert input into lowercase 
           present_count+=1
     
          
           
     attendance_count+=1   
 
+#displaying the number of present student and absent student in the class 
 
 print("No of present student:",present_count) 
 print("no of absent student:",total_students-present_count)
