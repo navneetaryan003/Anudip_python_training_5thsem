fb479f06f5cbb0e7979d27509ae6c14276ad56b6
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 16:17:57 +0530

    strong number checker

--- Labwork/iteration_4th-June/strong_number.py
+++ Labwork/iteration_4th-June/strong_number.py
@@ -1,9 +1,18 @@
 #enter a number
 number=int(input("enter the number:"))
+
+#validating that number should not be negative
 if(number<0):
+     
+     #exit from the execution 
+
      exit("number is negative....exited")
 
+   #making a copy of the number  
+
 temp=number
+
+#initialisation of sum
 sum=0
 
 
@@ -12,15 +21,20 @@ while(temp>0):
     #calculating digits of a number
     
     digit=temp%10
+
+    #initialisation of factorial 
+
     fact=1
 
     #calculating factor of the digit
     for i in range(1,digit+1):
            fact=fact*i
 
-     #calculating sum of the factors of the digit
-     #       
+     #calculating sum of the factors of the digit 
     sum=sum+fact
+
+    #updating the number for next iteration
+    
     temp=temp//10
 
     #checking the number is strong number or not 
