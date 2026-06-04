4e4258ed5295b1f295d4634df9a3d2047cfd260d
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 11:52:20 +0530

    classwork

---
+++ classwork/iteration_4th-June/Attendance_tracker.py
@@ -0,0 +1,24 @@
+#write a program to count the no of student present or absent
+
+total_students=5
+attendance_count=1
+present_count=0
+
+while(attendance_count<=5):
+    
+    print("student",attendance_count)
+    
+    #input for student present or not 
+    student_present=input("Attendance:")
+
+    #count the present student
+    
+    if(student_present.lower()=="present"):
+          present_count+=1
+    
+         
+          
+    attendance_count+=1   
+
+print("No of present student:",present_count) 
+print("no of absent student:",total_students-present_count)
