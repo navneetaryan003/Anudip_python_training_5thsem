3e7fe60cb20d8a32d1184c253ca300ae9a2fc009
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 16:19:57 +0530

    electricity bill calculator

--- Labwork/iteration_4th-June/electricity_bill_calculator.py
+++ Labwork/iteration_4th-June/electricity_bill_calculator.py
@@ -14,17 +14,22 @@ print("units consumed",consumed_units)
 #also calculating the total bill of the user 
 
 if(consumed_units<=100):
+
     Total_bill=consumed_units*5
-    print("Total Bill",Total_bill)
+
+    print("Total Bill",Total_bill) #displaying bill 
     print("Low Consumed")
 
 elif(consumed_units<=200):
 
     Total_bill=100*5+((consumed_units-100)*7)
-    print("Total Bill",Total_bill)
+
+    print("Total Bill",Total_bill) #displaying bill
     print("Moderate consumed") 
     
 else:
     Total_bill=(100*5)+(100*7)+((consumed_units-200)*10)
-    print("Total bill:",Total_bill)
+
+    print("Total bill:",Total_bill) #displaying bill
+    
     print("High consumed")
