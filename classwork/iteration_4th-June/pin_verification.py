127755a76ac832b9af3229387a44cc6c75363491
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 20:53:14 +0530

    pin verfication

--- classwork/iteration_4th-June/pin_verification.py
+++ classwork/iteration_4th-June/pin_verification.py
@@ -1,6 +1,8 @@
 #write a program to validate the pin 
 
 valid_pin="1234"
+
+#initialisation of attempt 
 attempt=0
 
 while(True):
@@ -8,18 +10,23 @@ while(True):
 
  pin=input("enter pin:")
 
-#  validate the pin
+#  validate the pin 
+
  if(pin==valid_pin):
   print("access granted")
   break
+ 
  else:
   #  checking the pin is four digit or not
 
    if(len(pin)!= 4 ):
     print("pin is not acceptable")
     continue
+
    else:
     print("incorrectPIN:Try Again")
    attempt+=1
+   
+   #displaying the number of attempt to validate the pin
    
 print("Total failed attempt:",attempt)
