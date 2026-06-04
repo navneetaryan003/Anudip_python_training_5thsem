b5d4852752469c8a2302c78f65c25123d61fb463
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 21:04:40 +0530

    battery charging tracking

--- classwork/iteration_4th-June/battery_charging_level.py
+++ classwork/iteration_4th-June/battery_charging_level.py
@@ -1,6 +1,12 @@
-charging_level=20
-electricity_status=True
+#track the battery charging percentage 
+
+charging_level=20        #initialisation of initial battery percentage 
+
+
 while (charging_level<=100):
-    print("battery level:",charging_level,"%")
-    charging_level=charging_level+10
+
+    print("battery level:",charging_level,"%") #displaying current charging percentage
+
+    charging_level=charging_level+10    #increment in charging in every cycle 
+    
 print("battery full charged")
