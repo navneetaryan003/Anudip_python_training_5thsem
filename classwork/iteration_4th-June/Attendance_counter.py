4dfdd312ad2e2f3c77aee0a526f87254d05262d2
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 20:58:07 +0530

    attendance counter

--- classwork/iteration_4th-June/Attendance_counter.py
+++ classwork/iteration_4th-June/Attendance_counter.py
@@ -1,7 +1,18 @@
+#write a program to count the attendance of the student
+#initialisation total number of students
+
 total_counter=30
+
+
+
+#counter to track the attendance of student 
 attendance_counter=0
-while(attendance_counter<30):
+
+while(attendance_counter<30):      #condition to control the loop 
     print("student entered")
-    attendance_count+=1
+    attendance_count+=1            #increment 
+    
+     #displaying the attencdance
+
     print("attendance count:",attendance_count)
 print("all students are present")
