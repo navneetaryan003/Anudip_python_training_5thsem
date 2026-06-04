596f01cfdb90b24a20c886806fb1acd71f06223b
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 21:10:05 +0530

    shopping cart billing

--- classwork/iteration_4th-June/shoping_cart_bill.py
+++ classwork/iteration_4th-June/shoping_cart_bill.py
@@ -1,10 +1,15 @@
-total_bill=0
+#write a program to track or calculate the bill of the user 
+
+total_bill=0   #initialisation of bill ammount
+
 while(True):
     
-   item_added=int(input("enter item price"))
+   item_added=int(input("enter item price"))  #taking price of item that is added
    
-   if(item_added==0):
+   if(item_added==0):        #checking whether it is zero priced enetr by the user to exit 
         break
-   total_bill=total_bill+item_added
+   
+   total_bill=total_bill+item_added   #calculating total bill untill user enter zero pricing in the cart
+   
 print("total bill ammount",total_bill)
 
