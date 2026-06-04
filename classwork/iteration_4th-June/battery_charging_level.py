0ae171a43a400981722c687655f950acda625bf3
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-04 10:56:51 +0530

    classwork

---
+++ classwork/iteration_4th-June/battery_charging_level.py
@@ -0,0 +1,6 @@
+charging_level=20
+electricity_status=True
+while (charging_level<=100):
+    print("battery level:",charging_level,"%")
+    charging_level=charging_level+10
+print("battery full charged")
