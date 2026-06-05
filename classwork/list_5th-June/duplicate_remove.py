02b26f266519d9e07ca5475158bf85e01021dc19
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-05 14:30:08 +0530

    duplicate number remove

---
+++ classwork/list_5th-June/duplicate_remove.py
@@ -0,0 +1,38 @@
+numbers=[] #take a empty list
+
+for i in range(5):
+
+    num=int(input("enter the number")) #taking input from user 
+
+    numbers.append(num)     #add them to the list
+
+diff_number=int(input("enter the another number:"))   #taking different number from the user 
+
+
+counts=numbers.count(diff_number)     #count the target number how many times it occured 
+
+
+if(counts==0):
+    print(diff_number ,"is not found")
+
+
+elif(counts==1):
+    print("no duplicates found")
+    
+
+else:    
+
+    #reversing the list
+    numbers.reverse()
+    for i in range(1,counts):
+
+        # removing the duplicate element
+        numbers.remove(diff_number)
+    
+    numbers.reverse() #again reversing the list to make it in original form 
+
+print(numbers)
+    
+
+
+
