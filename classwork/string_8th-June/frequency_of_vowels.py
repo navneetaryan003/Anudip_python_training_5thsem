9fcd478ffed55962db976c8b4ef8839c39e5dd3d
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-08 15:24:04 +0530

    frequency of vowels

---
+++ classwork/string_8th-June/frequency_of_vowels.py
@@ -0,0 +1,21 @@
+#write a program to count the frequency of vowels in a given sentence.
+
+sentence = input("Enter a sentence: ")
+
+#vowels
+vowels = "aeiouAEIOU"
+
+#counting the frequency of vowels
+frequency = {}     #dictionary to store the frequency of each vowel
+for char in sentence:
+    if char in vowels:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+
+#displaying the frequency of vowels
+print("Frequency of vowels in the given sentence:")
+for vowel, count in frequency.items():
+    print(f"{vowel}: {count}")  
+    
