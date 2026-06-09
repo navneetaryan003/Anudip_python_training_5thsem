3cf5eefb2d8c4f3c76c9ee095c6ffd118cf87f5f
Navneet Aryan <navneetaryan003@gmail.com>
2026-06-09 15:38:43 +0530

    area and perimeter module

--- classwork/Function_in_python_9th-June/area_and_perimeter.py
+++ classwork/Function_in_python_9th-June/area_and_perimeter.py
@@ -1,32 +1,32 @@
 #creating module for calculating area and perimeter of square , rectangle and circle
 
 #function to calculate area  of square
-def square(side):
+def square_area(side):
     area=side*side
     return area
 
 #function to calculate perimeter of square
-def perimeter(side):
+def square_perimeter(side):
     perimeter=4*side
     return perimeter
 
 #function to calculate area of rectangle
-def rectangle(length,breadth):
+def rectangle_area(length,breadth):
     area=length*breadth
     return area
 
 #function to calculate perimeter of rectangle
-def perimeter(length,breadth):
+def rectangle_perimeter(length,breadth):
     perimeter=2*(length+breadth)
     return perimeter
 
 #function to calculate area of circle
-def circle(radius):
+def circle_area(radius):
     area=3.14*radius*radius
     return area 
 
 #function to calculate circumference of circle
-def circumference(radius):
+def circle_circumference(radius):
     circumference=2*3.14*radius
     return circumference
 
